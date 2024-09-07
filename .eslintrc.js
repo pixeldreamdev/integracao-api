3f250fd928f9e4d279adf9da37acaa54e2521302
pixeldreamdev <pixeldreamdev@outlook.com>
2024-09-07 16:55:33 -0300

    atualizacao eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: 'babel-eslint', // Mova o parser para fora das regras
   extends: [
     'next/core-web-vitals',
     'plugin:prettier/recommended', // Adiciona o Prettier à configuração do ESLint
@@ -8,6 +9,5 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/no-unescaped-entities': 'off', // Desativa a regra que estava causando o erro anterior
     singleQuote: [2, 'always'],
-    parser: 'babel-eslint',
   },
 };
