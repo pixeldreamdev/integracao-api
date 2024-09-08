fdc1cbe1ec86ed53385e9a048189901388744794
pixeldreamdev <pixeldreamdev@outlook.com>
2024-09-07 23:20:20 -0300

    att projeto

--- src/app/lib/utils/validations.js
+++
@@ -1,6 +0,0 @@
-export const validatePropostaData = data => {
-  // Implemente a lógica de validação
-  // Retorne true se válido, ou um objeto com erros se inválido
-};
-
-// Outras funções de validação conforme necessário
