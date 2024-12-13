9e966aa94a07159d54a433643eca47fe4bddaac5
pixeldreamdev <pixeldreamdev@outlook.com>
2024-12-13 18:09:32 -0300

    att

--- src/app/utils/urlHelpers.js
+++ src/app/utils/urlHelpers.js
@@ -1,8 +1,12 @@
 export function getNotificationUrl() {
   const baseUrl =
     process.env.NEXT_PUBLIC_BASE_URL ||
-    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null);
 
-  console.log('Ambiente:', process.env.NEXT_PUBLIC_ENV || 'prod');
+  if (!baseUrl) {
+    throw new Error('Base URL não está configurada corretamente.');
+  }
+
+  console.log('Ambiente atual:', process.env.NEXT_PUBLIC_ENV || 'prod');
   return `${baseUrl}/api/notificacao`;
 }
