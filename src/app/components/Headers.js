d2a83b31992b3b791f7928dc31a87ca619f8f24d
pixeldreamdev <pixeldreamdev@outlook.com>
2025-02-05 18:01:21 -0300

    QS1

--- src/app/components/Headers.js
+++ src/app/components/Headers.js
@@ -60,7 +60,7 @@ const Header = () => {
   const menuItems = [
     { path: 'quem-somos', label: 'Quem Somos' },
     { path: '#services', label: 'Serviços' }, // Atualizado para scroll
-    { path: '/contato', label: 'Contato' },
+    { path: 'https://wa.me/5511958255717', label: 'Contato' },
     { path: '/autocontratacao', label: 'Simular Empréstimo' },
   ];
 
